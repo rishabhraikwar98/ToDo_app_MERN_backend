1358869fa5bb2a48cdc1f4e6760b378e643bc325
rishabhraikwar98 <rishabhraikwar77@gmail.com>
2023-12-04 13:33:46 +0530

    cors added

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors")
 require("dotenv").config();
 const mongoose = require("mongoose");
 const app = express();
@@ -6,6 +7,7 @@ const todoRouter = require("./routes/todo");
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
 app.use(express.json());
+app.use(cors())
 
 mongoose
   .connect(MONGO_URI)
